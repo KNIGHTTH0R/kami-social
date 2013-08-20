719d135e4a919c20e3b5634c5a2dbde88f028d2e
Kami Nguyen <si.gn.up.kp@gmail.com>
2013-08-20 10:09:39 +0700

    clicking on username will link to the original post, not the user profile

--- index.php
+++ index.php
@@ -370,7 +370,7 @@ if ($(window).scrollTop() >= ($(document).height() - $(window).height())*0.75){
           <div class="post">
             <div class="uinfo">
                 <div class="post-pic"><img src="https://graph.facebook.com/<?php echo $uid; ?>/picture"></div>
-                <a href="https://www.facebook.com/<?php echo $from['id']; ?>/"><?php echo $item_count . ") " . $name; ?>:</a><br/>
+                <a href="https://www.facebook.com/<?php echo $from['id']; ?>/posts/<?php echo $id; ?>"><?php echo $item_count . ") " . $name; ?>:</a><br/>
                 <label class="lblhdr">Username</label>: <?php echo $username;?><br/>
                 <label class="lblhdr">Gender</label>: <?php echo $gender;?><br/>
                 <label class="lblhdr">Location</label>: <?php echo $add;?><br/>
